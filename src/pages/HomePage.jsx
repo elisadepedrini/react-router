00054c5789a8cf363ad6d44672fb6faa7a5d4c39
elisadepedrini <elisa.depedrini@gmail.com>
2026-05-17 11:43:15 +0200

    Create ProductsPage and fix HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import AppHeader from "../components/AppHeader";
-import ProductsPage from "./ProductsPage";
-import WhoPage from "./WhoPage";
+
 
 export default function HomePage() {
 
@@ -10,17 +9,17 @@ export default function HomePage() {
         <AppHeader />
 
         <main>
-            <div class="p-5 mb-4 bg-light rounded-3 d-flex flex-row">
-                <div class="container-fluid py-5">
-                    <h1 class="display-5 fw-bold w-50 pb-4">Design che ispira ogni giorno</h1>
-                    <p class="col-md-8 fs-4 w-100">
+            <div className="p-5 mb-4 bg-light rounded-3 d-flex flex-row">
+                <div className="container-fluid py-5">
+                    <h1 className="display-5 fw-bold w-50 pb-4">Design che ispira ogni giorno</h1>
+                    <p className="col-md-8 fs-4 w-100">
                         Oggetti selezionati con cura per portare bellezza e funzionalità nella tua casa. Artigianato autentico, materiali premium.
                     </p>
                     
-                    <button class="btn btn-primary btn-md list-unstyled me-3" type="button">
+                    <button className="btn btn-primary btn-md list-unstyled me-3" type="button">
                         <Link to="/products" className="text-light text-decoration-none">Scopri la collezione</Link>
                     </button>  
-                    <button class="btn btn-primary btn-md" type="button">
+                    <button className="btn btn-primary btn-md" type="button">
                         <Link to="/who" className="text-light text-decoration-none">Chi siamo</Link>
                     </button>  
                     
@@ -28,11 +27,11 @@ export default function HomePage() {
 
 
 
-                <div class="card text-start text-center">
-                    <img class="card-img-top w-25 mx-auto mt-5" src="https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_t.png" alt="Title" />
-                    <div class="card-body">
-                        <h4 class="card-title mt-5">White Gold Plated Princess</h4>
-                        <p class="card-text mt-4"><em>Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day..."</em></p>
+                <div className="card text-start text-center">
+                    <img className="card-img-top w-25 mx-auto mt-5" src="https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_t.png" alt="Title" />
+                    <div className="card-body">
+                        <h4 className="card-title mt-5">White Gold Plated Princess</h4>
+                        <p className="card-text mt-4"><em>Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day..."</em></p>
                     </div>
                 </div>
             </div>
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,11 +1,43 @@
+import { useEffect, useState } from "react";
 import AppHeader from "../components/AppHeader";
 
 export default function ProductsPage() {
+  const api_url = "https://fakestoreapi.com/products";
+  const [products, setProducts] = useState([]);
 
-    return(
-        <>
-        <AppHeader />
-        <h1>Products Page</h1>
-        </>
-    )
+  useEffect(() => {
+    fetch(api_url)
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  }, []);
+
+  return (
+    <>
+      <AppHeader />
+
+      <main>
+        <div className="container text-center m-4 row">
+          {products.map((product) => (
+            <div className="card col-4 p-4" key={product.id}>
+              <img
+                className="card-img-top w-25 mx-auto mb-4"
+                src={product.image}
+                alt="Card image cap"
+              />
+              <div className="card-body">
+                <h4 className="card-title mb-3">{product.title}</h4>
+                <p className="card-text">{product.description}</p>
+              </div>
+              <div className="d-flex justify-content-between">
+                <h4 className="price">{product.price} €</h4>
+                <button type="button" className="btn btn-primary py-1 px-3"><strong>+</strong></button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+    </>
+  );
 }
